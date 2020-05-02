fad8b51bc8382fdb72628518a9df1373fae78084
sudhakarshah <sudhakarshah2013@gmail.com>
2020-05-02 12:49:01 +0530

    clean code and remove comments

--- host/main.c
+++ host/main.c
@@ -1,9 +1,3 @@
-/*
- * server.c
- * Version 20161003
- * Written by Harry Wong (RedAndBlueEraser)
- */
-
 #include <netinet/in.h>
 #include <pthread.h>
 #include <signal.h>
@@ -14,6 +8,7 @@
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
+
 /* OP-TEE TEE client API (built by optee_client) */
 #include <tee_client_api.h>
 
@@ -26,13 +21,14 @@
 pthread_mutex_t lock1;
 
 typedef struct pthread_arg_t {
-    int new_socket_fd;
-    struct sockaddr_in client_address;
-    /* TODO: Put arguments passed to threads here. See lines 116 and 139. */
+  int new_socket_fd;
+  struct sockaddr_in client_address;
 } pthread_arg_t;
 
+// Counters for debugging
 uint32_t total = 0;
 uint32_t curr = 0;
+
 /* Thread routine to serve connection to client. */
 void *pthread_routine(void *arg);
 
@@ -80,9 +76,8 @@ void *pthread_routine(void *arg)
 
   free(arg);
 
- // printf("client thread\n");
-
   int n; 
+
   // infinite loop for chat 
   for (;;) { 
     char buff[MAX]; 
@@ -91,24 +86,22 @@ void *pthread_routine(void *arg)
     // read the message from client and copy it in buffer 
     ssize_t cc =    read(sockfd, buff, sizeof(buff)); 
 
-    if (cc <=0) {
-
+    if (cc <= 0) {
       printf("empty buffer with value %d\n", cc);
       break;
     }
 
-    // print buffer which contains the client contents 
-    //printf("From client: %c\n", buff[1]);
+    // Parsing msg sent by the client
     int i = 4;
     int a = 0;
+
     while (buff[i] != ' ')
     {	
       int temp = buff[i] - '0';
       a = a*10 + temp;
       i++;	
-
     }
-    printf("a is: %d\n", a);  
+
     i++;
     int b = 0;
 
@@ -128,162 +121,145 @@ void *pthread_routine(void *arg)
     op.params[2].tmpref.buffer = second;
     op.params[2].tmpref.size = sizeof(second);
 
+
+    // Excuting msg according to the command recieved
     if (strncmp(buff, "ADD", 3) == 0) {
       res = TEEC_InvokeCommand(&sess, TA_ADD, 
-                              &op, &err_origin);
-    
+          &op, &err_origin);
     }
     else if (strncmp(buff, "SUB", 3) == 0) {
       res = TEEC_InvokeCommand(&sess, TA_SUBTRACT, 
-                              &op, &err_origin);
-    }
-    else if (strncmp(buff, "MUL", 3) == 0) {
+          &op, &err_origin);
+    } else if (strncmp(buff, "MUL", 3) == 0) {
       res = TEEC_InvokeCommand(&sess, TA_MULTIPLY, 
-                              &op, &err_origin);
+          &op, &err_origin);
     }
     else {
       printf("Incorrect operation send to the OPTEE server");
 
     }
-    
+
     if (res != TEEC_SUCCESS)
       errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
           res, err_origin);
 
-
     int ans = third [0];
-    //printf(" third is %d\n", third[0]);
 
     bzero(buff, MAX); 
     snprintf(buff, sizeof(buff), "%d\n", ans);
 
-
-    //printf("ans is: %d\n", ans);  
     write(sockfd, buff, sizeof(buff)); 
-
 
     bzero(buff, MAX); 
   } 
 
-
   close(sockfd);
 
-	pthread_mutex_lock(&lock1);
-	TEEC_CloseSession(&sess);
+  pthread_mutex_lock(&lock1);
+  TEEC_CloseSession(&sess);
 
   curr = curr - 1;
   TEEC_FinalizeContext(&ctx);
-	pthread_mutex_unlock(&lock1);
-  
-  return NULL;
+  pthread_mutex_unlock(&lock1);
 
+  return NULL;
 }
 
 /* Signal handler to handle SIGTERM and SIGINT signals. */
 void signal_handler(int signal_number);
 
 int main(int argc, char *argv[]) {
-    int port, socket_fd, new_socket_fd;
-    struct sockaddr_in address;
-    pthread_attr_t pthread_attr;
-    pthread_arg_t *pthread_arg;
-    pthread_t pthread;
-    socklen_t client_address_len;
 
-    /* Get port from command line arguments or stdin. */
-    port = 12345; 
+  int port, socket_fd, new_socket_fd;
+  struct sockaddr_in address;
+  pthread_attr_t pthread_attr;
+  pthread_arg_t *pthread_arg;
+  pthread_t pthread;
+  socklen_t client_address_len;
 
-    /* Initialise IPv4 address. */
-    memset(&address, 0, sizeof address);
-    address.sin_family = AF_INET;
-    address.sin_port = htons(port);
-    address.sin_addr.s_addr = INADDR_ANY;
+  /* Get port from command line arguments or stdin. */
+  port = 12345; 
 
-    /* Create TCP socket. */
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
+  /* Initialise IPv4 address. */
+  memset(&address, 0, sizeof address);
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  address.sin_addr.s_addr = INADDR_ANY;
+
+  /* Create TCP socket. */
+  if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+
+  /* Bind address to socket. */
+  if (bind(socket_fd, (struct sockaddr *)&address, sizeof address) == -1) {
+    perror("bind");
+    exit(1);
+  }
+
+  /* Listen on socket. */
+  if (listen(socket_fd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  /* Assign signal handlers to signals. */
+  if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+    perror("signal");
+    exit(1);
+  }
+  if (signal(SIGTERM, signal_handler) == SIG_ERR) {
+    perror("signal");
+    exit(1);
+  }
+  if (signal(SIGINT, signal_handler) == SIG_ERR) {
+    perror("signal");
+    exit(1);
+  }
+
+  /* Initialise pthread attribute to create detached threads. */
+  if (pthread_attr_init(&pthread_attr) != 0) {
+    perror("pthread_attr_init");
+    exit(1);
+  }
+  if (pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
+    perror("pthread_attr_setdetachstate");
+    exit(1);
+  }
+
+  while (1) {
+
+    pthread_arg = (pthread_arg_t *)malloc(sizeof *pthread_arg);
+    if (!pthread_arg) {
+      perror("malloc");
+      continue;
     }
 
-    /* Bind address to socket. */
-    if (bind(socket_fd, (struct sockaddr *)&address, sizeof address) == -1) {
-        perror("bind");
-        exit(1);
+    /* Accept connection to client. */
+    client_address_len = sizeof pthread_arg->client_address;
+    new_socket_fd = accept(socket_fd, (struct sockaddr *)&pthread_arg->client_address, &client_address_len);
+
+    if (new_socket_fd == -1) {
+      perror("accept");
+      free(pthread_arg);
+      continue;
     }
 
-    /* Listen on socket. */
-    if (listen(socket_fd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
+    /* Initialise pthread argument. */
+    pthread_arg->new_socket_fd = new_socket_fd;
 
-    /* Assign signal handlers to signals. */
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
-        perror("signal");
-        exit(1);
+    /* Create thread to serve connection to client. */
+    if (pthread_create(&pthread, &pthread_attr, pthread_routine, (void *)pthread_arg) != 0) {
+      perror("pthread_create");
+      free(pthread_arg);
+      continue;
     }
-    if (signal(SIGTERM, signal_handler) == SIG_ERR) {
-        perror("signal");
-        exit(1);
-    }
-    if (signal(SIGINT, signal_handler) == SIG_ERR) {
-        perror("signal");
-        exit(1);
-    }
+  }
 
-    /* Initialise pthread attribute to create detached threads. */
-    if (pthread_attr_init(&pthread_attr) != 0) {
-        perror("pthread_attr_init");
-        exit(1);
-    }
-    if (pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
-        perror("pthread_attr_setdetachstate");
-        exit(1);
-    }
-
-    while (1) {
-        /* Create pthread argument for each connection to client. */
-        /* TODO: malloc'ing before accepting a connection causes only one small
-         * memory when the program exits. It can be safely ignored.
-         */
-        pthread_arg = (pthread_arg_t *)malloc(sizeof *pthread_arg);
-        if (!pthread_arg) {
-            perror("malloc");
-            continue;
-        }
-	
-        /* Accept connection to client. */
-        client_address_len = sizeof pthread_arg->client_address;
-        new_socket_fd = accept(socket_fd, (struct sockaddr *)&pthread_arg->client_address, &client_address_len);
-        if (new_socket_fd == -1) {
-            perror("accept");
-            free(pthread_arg);
-            continue;
-        }
-
-        //printf("client port %d\n", new_socket_fd);
-        /* Initialise pthread argument. */
-        pthread_arg->new_socket_fd = new_socket_fd;
-        /* TODO: Initialise arguments passed to threads here. See lines 22 and
-         * 139.
-         */
-
-        /* Create thread to serve connection to client. */
-        if (pthread_create(&pthread, &pthread_attr, pthread_routine, (void *)pthread_arg) != 0) {
-            perror("pthread_create");
-            free(pthread_arg);
-            continue;
-        }
-    }
-
-    /* close(socket_fd);
-     * TODO: If you really want to close the socket, you would do it in
-     * signal_handler(), meaning socket_fd would need to be a global variable.
-     */
-    return 0;
+  return 0;
 }
 
 void signal_handler(int signal_number) {
-    /* TODO: Put exit cleanup code here. */
-    exit(0);
+  exit(0);
 }
